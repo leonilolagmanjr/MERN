b0fc8b610b388474eb2537a30a092757f202318d
leonilolagmanjr <156587139+leonilolagmanjr@users.noreply.github.com>
2025-10-14 09:09:17 +0800

    chat api

--- frontend/src/services/chatApi.js
+++ frontend/src/services/chatApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://100.123.122.74:5000/api/chat', // adjust if needed
+  baseURL: process.env.SOCKET_URL // adjust if needed
 });
 
 // Add token to requests
